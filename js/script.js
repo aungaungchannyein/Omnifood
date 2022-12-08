f55e774a70313a050fe30d1e3c48f92a32ca4e8b
aungaungchannyein <aungaung.channyein@gmail.com>
2022-12-08 21:42:37 +0630

    complete

--- js/script.js
+++ js/script.js
@@ -29,6 +29,24 @@ allLinks.forEach(function(link){
   })
 })
 
+///////////////////////////////////////////////////////////
+// Sticky navigation
+const sectionheroEl= document.querySelector(".section-hero");
+const obs= new IntersectionObserver(function(entries){
+  const ent= entries[0];
+  if(ent.isIntersecting === false){
+    document.body.classList.add("sticky");
+  }
+  if(ent.isIntersecting){
+    document.body.classList.remove("sticky");
+  }
+},{
+  // In the viewport
+  root: null,
+  threshold:0,
+  rootMargin: '-80px',
+})
+obs.observe(sectionheroEl);
 
 
 
